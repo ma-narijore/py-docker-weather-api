20a6a5514e028a165d0c21d7e171fc1b5908811c
Oleh-Fr <frasyniuk.oleh.dev@gmail.com>
2026-01-18 20:55:42 +0000

    fix with AI v2.0

--- app/main.py
+++ app/main.py
@@ -2,16 +2,12 @@ import os
 
 import requests
 
-from dotenv import load_dotenv
-
-load_dotenv()
-
-
 BASE_URL = "http://api.weatherapi.com/v1"
 
 ENDPOINT = "/current.json"
 
 CITY = "Paris"
+
 
 def get_weather() -> None:
     req = requests.get(
@@ -22,12 +18,14 @@ def get_weather() -> None:
         },
         params={
             "q": CITY,
-        }
+        },
     ).json()
 
     print(f"Performing request to Weather API for city {req["location"]["name"]}...")
-    print(f"{req['location']['name']}/{req['location']['country']} {req['location']['localtime']} "
-          f"Weather: {req['current']['temp_c']} Celsius, {req['current']['condition']["text"]}.")
+    print(
+        f"{req['location']['name']}/{req['location']['country']} {req['location']['localtime']} "
+        f"Weather: {req['current']['temp_c']} Celsius, {req['current']['condition']["text"]}."
+    )
 
 
 if __name__ == "__main__":
